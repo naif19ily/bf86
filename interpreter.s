8508577d6c76e33436d118d7947d867707f62905
naif19ily <juan.patino.dc@gmail.com>
2025-04-15 12:43:10 -0500

    checkpoint

--- interpreter.s
+++ interpreter.s
@@ -8,15 +8,15 @@ interpreter:
 	leaq	TOKEN_STREAM(%rip), %r8
 	leaq	MEMORY(%rip), %r9
 .loop:
-	xorq	%rcx, %rcx
 	movq	(%r8), %rax
 	cmpq	$0, %rax
 	je	.c_fini
+	xorq	%rcx, %rcx
 	movzbl	(%rax), %eax
 	cmpb	$'+', %al
 	je	.inst_inc
 	cmpb	$'-', %al
-	je	.inst_inc
+	je	.inst_dec
 	cmpb	$'>', %al
 	je	.inst_nxt
 	cmpb	$'<', %al
@@ -32,38 +32,50 @@ interpreter:
 	jmp	.continue
 
 .inst_inc:
-	movl	24(%r8), %eax
-	addb	%al, (%r9)
+	movl	24(%r8), %ecx
+	addb	%cl, (%r9)
 	jmp	.continue
 
 .inst_dec:
-	movl	24(%r8), %eax
-	subb	%al, (%r9)
+	movl	24(%r8), %ecx
+	subb	%cl, (%r9)
 	jmp	.continue
 
 .inst_nxt:
-	incq	%r9
+	movl	24(%r8), %eax
+	cltq
+	addq	%rax, %r9
 	jmp	.continue
 
 .inst_prv:
-	decq	%r9
+	movl	24(%r8), %eax
+	cltq
+	subq	%rax, %r9
 	jmp	.continue
 
 .inst_out:
+	movl	24(%r8), %eax
+	cmpl	$0, %eax
+	je	.continue
 	movq	$1, %rax
 	movq	$1, %rdi
 	movq	%r9, %rsi
 	movq	$1, %rdx
 	syscall
-	jmp	.continue
+	decl	24(%r8)
+	jmp	.inst_out
 
 .inst_inp:
+	movl	24(%r8), %eax
+	cmpl	$0, %eax
+	je	.continue
 	movq	$0, %rax
 	movq	$0, %rdi
 	movq	%r9, %rsi
 	movq	$1, %rdx
 	syscall
-	jmp	.continue
+	decl	24(%r8)
+	jmp	.inst_out
 
 .inst_opn:
 	movzbl	(%r9), %eax
@@ -73,9 +85,11 @@ interpreter:
 	cltq
 	movq	TOKEN_SIZE(%rip), %rbx
 	mulq	%rbx
-	leaq	TOKEN_STREAM(%rip), %r8
-	addq	%rax, %r8
-	jmp	.continue
+	movq	%rax, %rbx
+	leaq	TOKEN_STREAM(%rip), %rax
+	addq	%rbx, %rax
+	movq	%rax, %r8
+	jmp	.loop
 
 .inst_cls:
 	movzbl	(%r9), %eax
@@ -85,9 +99,11 @@ interpreter:
 	cltq
 	movq	TOKEN_SIZE(%rip), %rbx
 	mulq	%rbx
-	leaq	TOKEN_STREAM(%rip), %r8
-	addq	%rax, %r8
-	jmp	.continue
+	movq	%rax, %rbx
+	leaq	TOKEN_STREAM(%rip), %rax
+	addq	%rbx, %rax
+	movq	%rax, %r8
+	jmp	.loop
 
 .continue:
 	movq	TOKEN_SIZE(%rip), %rax
